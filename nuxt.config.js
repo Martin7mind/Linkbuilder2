7ffae603b0237dd38bfb3925057cc81e4314ab34
Martin Duveborg <martin@7mind.de>
2020-02-10 10:45:51 +0100

    github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/Linkbuilder2/'
+  }
+} : {}
+
 export default {
+  ...routerBase,
   mode: 'spa',
   /*
   ** Headers of the page
